3ed464404c56fcb926b38a7b0c268491f429180c
Adrian Wright <adrian@compiledcoding.co.uk>
2021-10-15 13:52:59 +0100

    Update tests to use new classes and to test the standard unit prices.

---
+++ PromotionEngine.Tests/WithNoPromotions.cs
@@ -0,0 +1,69 @@
+using PromotionEngine.Shared;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PromotionEngine.Tests
+{
+    public class WithNoPromotions
+    {
+        [Fact]
+        public void OneUnitOfProductAWithNoPromotionAppliedHasCorrectTotal()
+        {
+            var orderItems = new List<ProductBase>
+            {
+                new ProductA()
+            };
+
+            //ARRANGE
+            var order = new Order<ProductBase>(orderItems);
+
+            //ASSERT
+            Assert.Equal(50, order.Total);
+        }
+
+        [Fact]
+        public void OneUnitOfProductBWithNoPromotionAppliedHasCorrectTotal()
+        {
+            var orderItems = new List<ProductBase>
+            {
+                new ProductB()
+            };
+
+            //ARRANGE
+            var order = new Order<ProductBase>(orderItems);
+
+            //ASSERT
+            Assert.Equal(30, order.Total);
+        }
+
+        [Fact]
+        public void OneUnitOfProductCWithNoPromotionAppliedHasCorrectTotal()
+        {
+            var orderItems = new List<ProductBase>
+            {
+                new ProductC()
+            };
+
+            //ARRANGE
+            var order = new Order<ProductBase>(orderItems);
+
+            //ASSERT
+            Assert.Equal(20, order.Total);
+        }
+
+        [Fact]
+        public void OneUnitOfProductDWithNoPromotionAppliedHasCorrectTotal()
+        {
+            var orderItems = new List<ProductBase>
+            {
+                new ProductD()
+            };
+
+            //ARRANGE
+            var order = new Order<ProductBase>(orderItems);
+
+            //ASSERT
+            Assert.Equal(15, order.Total);
+        }
+    }
+}
